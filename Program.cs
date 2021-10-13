e20d382adb74e60c289222a6605a095339fbc20b
henriqueholtz <henrique_holtz@hotmail.com>
2021-10-13 18:39:39 -0300

    Add swagger (only active when the key "enable-swagger" is "true" into appsettings.json)

--- Program.cs
+++ Program.cs
@@ -1,14 +1,27 @@
+using System.Collections.Immutable;
 using MinimalApi_SendSlackMessage.Model;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Swagger
+bool swagger = Boolean.TrueString.Equals(builder.Configuration.GetSection("enable-swagger").Value, StringComparison.InvariantCultureIgnoreCase);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment()) app.UseDeveloperExceptionPage();
+if (swagger) app.UseSwagger();
+
 
 app.MapGet("/", (IConfiguration Configuration) =>
 {
     return new { 
         Message = "Welcome to SendSlackMessage - The Minimal Api from dotnet 6!", 
         OSDescription = System.Runtime.InteropServices.RuntimeInformation.OSDescription,
-        Test = Configuration["test"]
+        Test = Configuration["test"],
+        EnalbeSwagger = swagger
     };
 });
 
@@ -20,4 +33,5 @@ app.MapPost("/send", (Message message) =>
     return Results.BadRequest(response);
 }).Produces<Response>();
 
+if (swagger) app.UseSwaggerUI();
 app.Run();
